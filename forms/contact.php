d05598c4e5f59d043e8a983934f444ba82945608
Alvarog0218 <122694377+Alvarog0218@users.noreply.github.com>
2023-04-23 14:35:36 -0500

    correo prueba

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'servicioalcliente@escanografiasa.com.co';
+  $receiving_email_address = 'alvarog0218@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
