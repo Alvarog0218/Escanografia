d088bffc03f9edad32a8c992ea209327b68fc346
Alvarog0218 <122694377+Alvarog0218@users.noreply.github.com>
2023-05-04 11:55:25 -0500

    php

--- forms/php-email-form.php
+++ forms/php-email-form.php
@@ -1,1 +1,25 @@
 <?php
+if (isset($_POST['enviar'])) {
+	$nombre = $_POST['nombre'];
+	$correo = $_POST['correo'];
+	$asunto = $_POST['asunto'];
+	$mensaje = $_POST['mensaje'];
+
+	// Formatear el correo electrónico
+	$para = 'alvarog0218@gmail.com'; // Reemplazar con la dirección de correo electrónico del destinatario
+	$titulo = 'Solicitud de cita por pagina web';
+	$contenido = "Nombre: $nombre\nCorreo: $correo\nAsunto: $asunto\nMensaje: $mensaje";
+
+	// Enviar el correo electrónico
+	if (mail($para, $titulo, $contenido)) {
+		// El correo electrónico se ha enviado con éxito
+
+		// Redirigir al usuario a una página de confirmación
+		header('Location: confirmacion.php');
+		exit;
+	} else {
+		// El correo electrónico no se ha podido enviar
+		echo 'Lo siento, ha habido un error al enviar el correo electrónico.';
+	}
+}
+?>
